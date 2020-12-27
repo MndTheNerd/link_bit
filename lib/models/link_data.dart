f3987c28c1bb574f0bc78c8e15f179f470a1f756
mndthenerd <mndthenerd@gmail.com>
2020-12-27 20:43:43 +0300

    using the firebase data to display it on the app NOTE: reverted back to older firestore version due to known issue in the latest version

--- lib/models/link_data.dart
+++ lib/models/link_data.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 
 class LinkData {
   final String title;
   final String url;
 
   LinkData({@required this.title, @required this.url});
+
+  factory LinkData.fromMap(Map<String, dynamic> data){
+    return LinkData(title: data['title'], url: data['url'],);
+  }
 }
--- lib/screens/links_landing_screen.dart
+++ lib/screens/links_landing_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:link_bit/models/link_data.dart';
 import '../components/footer.dart';
 import '../components/button_link.dart';
 import 'package:link_bit/constants.dart';
@@ -7,12 +8,18 @@ import 'package:link_bit/constants.dart';
 class LinksLandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //using streamBuilder to connect to firebase
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection('links').snapshots(),
       builder: (context, snapshot) {
+        //using a progress indicator while the data loads
         if(snapshot.connectionState == ConnectionState.waiting){
            return Center(child: CircularProgressIndicator(),);
         }
+        //grabbing the data from firebase and convert it according to the data type used
+        final _documents = snapshot.data.docs.map((doc) {
+          return LinkData.fromMap(doc.data()) ;
+        }).toList();
         return Material(
           child: Column(
             children: [
@@ -34,7 +41,7 @@ class LinksLandingPage extends StatelessWidget {
               ),
               SizedBox(height: 20),
               //go through a document map and add button for each one
-              for (var document in documents)
+              for (var document in _documents)
                 ButtonLink(
                   title: document.title,
                   urlString: document.url,
