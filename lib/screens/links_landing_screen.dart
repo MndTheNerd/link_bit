a1a014423e5b6417abebba0b2898a102fd1b370f
mndthenerd <mndthenerd@gmail.com>
2020-12-27 20:20:50 +0300

    using StreamBuilder , Connecting the firebase data to the app

--- lib/screens/links_landing_screen.dart
+++ lib/screens/links_landing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../components/footer.dart';
 import '../components/button_link.dart';
@@ -6,36 +7,44 @@ import 'package:link_bit/constants.dart';
 class LinksLandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          SizedBox(height: 35),
-          CircleAvatar(
-            backgroundColor: Colors.white,
-            backgroundImage: NetworkImage(imageURL),
-            radius: 42,
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('links').snapshots(),
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting){
+           return Center(child: CircularProgressIndicator(),);
+        }
+        return Material(
+          child: Column(
+            children: [
+              SizedBox(height: 35),
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: NetworkImage(imageURL),
+                radius: 42,
+              ),
+              SizedBox(height: 12),
+              Text(
+                '@MndTheNerd',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Developer, Content Creator and a Gamer',
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+              SizedBox(height: 20),
+              //go through a document map and add button for each one
+              for (var document in documents)
+                ButtonLink(
+                  title: document.title,
+                  urlString: document.url,
+                ),
+              Spacer(),
+              Footer()
+            ],
           ),
-          SizedBox(height: 12),
-          Text(
-            '@MndTheNerd',
-            style: TextStyle(fontSize: 14, color: Colors.grey),
-          ),
-          SizedBox(height: 8),
-          Text(
-            'Developer, Content Creator and a Gamer',
-            style: TextStyle(fontSize: 12, color: Colors.grey),
-          ),
-          SizedBox(height: 20),
-          //go through a document map and add button for each one
-          for (var document in documents)
-            ButtonLink(
-              title: document.title,
-              urlString: document.url,
-            ),
-          Spacer(),
-          Footer()
-        ],
-      ),
+        );
+      }
     );
   }
 }
